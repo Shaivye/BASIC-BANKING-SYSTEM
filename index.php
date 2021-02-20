9450c19d4d26527592ddcb41a3d02e80c35a12b0
Shaivye <63388959+Shaivye@users.noreply.github.com>
2021-02-20 13:25:53 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+
+    <title>Basic Banking System</title>
+  </head>
+
+  <body>
+  <?php
+  include 'navbar.php';
+  ?>
+  
+   
+      <!-- Introduction section -->
+            
+                <h1 style="text-align: center;">Basic Banking System</h1>
+<br>
+                
+              <div class="col-sm-12 col-md img text-center">
+                <img src="img/bank.png" class="img-fluid pt-2">
+              </div>
+            </div>
+
+
+            <div class="row activity text-center">
+                 
+                  <div class="col-md act">
+                   
+                    <br>
+                    <a href="transfermoney.php"><button>Transfer Money</button></a>
+                  </div>
+                 
+            </div>
+      </div>
+      <footer class="text-center mt-5 py-2">
+        <p>Made by Shaivye Singh</b> <br>Web Developer Intern @ TSF </p>
+      </footer>
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+  </body>
+</html>
